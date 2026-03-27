9dd91d70c889fc94d0bdb409b51f0656333f641a
Badsena <sena@amypo.in>
2026-03-27 10:15:05 +0000

    User commit

--- src/Todos.js
+++ src/Todos.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function Todos() {
   return (
     <div className="App">
       <header className="App-header">
@@ -15,4 +15,4 @@ function App() {
   );
 }
 
-export default App;
+export default Todos;
